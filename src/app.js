6735c5e88f16a9f0a2042698163adc167297a0a5
tu nombre de usuario <lean.k-rri@hotmail.com>
2022-12-15 20:06:57 -0300

    Mi Primer commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+// Requerimos express y lo ejecutamos para tener disponibles todos los metodos que vamos a precisar
+const express = require("express");
+const app = express();
+
+// Modulo nativo para manejar las rutas de los archivos
+const path = require("path");
+
+// Usando recursos estáticos.
+app.use(express.static("./public"));
+
+// Ponemos a escuchar el servidor
+app.listen(3033, () => {
+    console.log("Servidor corriendo en http://localhost:3033")
+});
+
+// Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/register.html", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.get("/login.html", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
